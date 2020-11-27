08032c72e5e237a0d3302578cfe1b244749ca459
Gitea <gitea@fake.local>
2020-11-27 12:05:43 +0800

    add go file

---
+++ app.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	fmt.Printf("Hello World!\n");
+	fmt.Printf("Hello World2222!\n");
+	fmt.Printf("Hello World333!\n");
+	fmt.Printf("Hello World444!\n");
+	fmt.Printf("Hello World555!\n");
+	fmt.Printf("Hello World666!\n");
+}
